5681291536b820dcd8075201d9c31230061fc270
kochkr <kochkar.roman@gmail.com>
2018-08-18 20:21:06 +0300

    Added kotlin-styled to the build with example, bump versions

--- src/main/kotlin/local/main.kt
+++ src/main/kotlin/local/main.kt
@@ -1,11 +1,29 @@
 package local
 
+import kotlinx.css.*
 import react.dom.div
 import react.dom.render
+import styled.StyleSheet
+import styled.css
+import styled.styledDiv
 import kotlin.browser.document
+
+object MainDivStyles : StyleSheet("MainDiv", isStatic = true) {
+    val wrapper by css {
+        textAlign = TextAlign.center
+        backgroundColor = Color("#CED9E0")
+        fontSize = 1.6.rem
+        fontWeight = FontWeight.w800
+    }
+}
 
 fun main(args: Array<String>) {
     render(document.getElementById("root")) {
-        div { +"Hello, Kotlin React Build" }
+        styledDiv {
+            css {
+                +MainDivStyles.wrapper
+            }
+            +"Hello, Kotlin React Build"
+        }
     }
 }
